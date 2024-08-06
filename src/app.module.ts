99fa628394a68e2569a8676b806520e6b4bb8135
Jude Kuti <judekuti@gmail.com>
2024-08-06 14:33:23 -0500

    Control flow form app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,34 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DevModule } from './dev/dev.module';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
+import { DevModule } from './dev/dev.module';
+import { DevService } from './dev/dev.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Dev } from './dev/dev.entity';
+import { DevRepository } from './dev/dev.repo';
+import { DevController } from './dev/dev.controller';
+import { RequestLoggerMiddleware } from './request-logger.middleware';
 @Module({
-  imports: [DevModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    DevModule,
+    TypeOrmModule.forFeature([Dev, DevRepository]),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [DevController],
+  providers: [DevService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggerMiddleware).forRoutes('*');
+  }
+}
